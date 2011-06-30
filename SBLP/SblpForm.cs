5f8f9d2a77edc0ba5f0053bdb3f7d864f1c7f339
Michael B. E. Rickert <nobody@nowhere.noplace>
2011-06-30 07:45:03 -0700

    Form skeleton

---
+++ SBLP/SblpForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SBLP {
+	[System.ComponentModel.DesignerCategory("")]
+	class SblpForm : Form {
+		SblpForm() {
+			DoubleBuffered = true;
+			Text = "SBLP";
+			FormBorderStyle = FormBorderStyle.None;
+			Bounds = Screen.PrimaryScreen.Bounds;
+			StartPosition = FormStartPosition.Manual;
+
+			Steps.Enqueue(0);
+			Steps.Enqueue(0);
+		}
+
+		static readonly Timer Timer = new Timer();
+
+		readonly Queue<int> Steps = new Queue<int>();
+
+		protected override void OnPaint( PaintEventArgs e ) {
+			Timer.Update();
+
+			var dt = Timer.dMilliseconds;
+			var fx = e.Graphics;
+			var w = ClientSize.Width;
+			var h = ClientSize.Height;
+
+			Steps.Enqueue(dt);
+			while ( Steps.Count>w ) Steps.Dequeue();
+
+			fx.Clear( Color.Black );
+			fx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+			fx.DrawLine( Pens.DarkGray, 0, h/2, w, h/2 );
+			fx.DrawLines( Pens.White, Steps.Select((ms,i)=>new Point(i,h/2-ms)).ToArray() );
+
+			base.OnPaint(e);
+		}
+
+		[STAThread] static void Main() {
+			using ( var form = new SblpForm() ) {
+				Application.Idle += (s,args) => form.Invalidate();
+				Application.Run(form);
+			}
+		}
+	}
+}
---
+++ SBLP/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SBLP {
+	public class Timer {
+		[DllImport("winmm.dll")] static extern uint timeEndPeriod( uint ms );
+		[DllImport("winmm.dll")] static extern uint timeBeginPeriod( uint ms );
+		[DllImport("winmm.dll")] static extern uint timeGetTime();
+
+		static readonly BindingFlags All = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+		static Timer() {
+			timeBeginPeriod(1);
+			Application.ApplicationExit += (s,args) => timeEndPeriod(1);
+		}
+
+		private uint previous;
+		public  int  dMilliseconds          { get; private set; }
+		public  long MillisecondsSinceStart { get; private set; }
+
+		public Timer() {
+			previous = timeGetTime();
+		}
+
+		public Timer( Timer toclone ) {
+			foreach ( var field in typeof(Timer).GetFields(All) ) field.SetValue( this, field.GetValue(toclone) );
+		}
+
+		public void Update() {
+			uint now = timeGetTime();
+			MillisecondsSinceStart += dMilliseconds = unchecked((int)(now-previous));
+			previous = now;
+		}
+	}
+}
